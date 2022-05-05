070edc7cca19824770ec8a0feec9184c8ba7dfad
natanbreaks <natanlucas771@gmail.com>
2022-05-04 21:31:14 -0300

    Atualiza nome

--- pokedex-app/src/pages/Home.js
+++ pokedex-app/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-
+import {useState} from 'react';
+import { useEffect } from "react";
 
 
 
@@ -10,6 +11,15 @@ import axios from "axios";
 const Home = () => {
 
   const navigate = useNavigate()
+
+  const [Pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+
+    getAllPokeName();
+  }, []);
+
+
   // botão para linka a pagina home a pagina da pokedex
   const IrParaPokedexPage = () => {
     navigate("/pokedex")
@@ -18,6 +28,30 @@ const Home = () => {
   const IrParaDetailsPage = () => {
     navigate("/details")
   }
+
+
+  const getAllPokeName = () => {
+
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?offset=151&limit=151")
+      .then((res) => {
+        setPokemons(res.data.results)
+        
+      })
+      .catch((err) => {
+        alert("[ERRO]")
+      })
+  };
+
+  const mapPokemons = Pokemons && Pokemons.map((pokemon)=>{
+
+    return(
+      <p> {pokemon.name} </p>
+      // <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    ) 
+
+  })
+  
 
   return (
 
@@ -30,6 +64,7 @@ const Home = () => {
       <div>
         {/* aki virao os cards com os pokemons , e terão 2 botoes de adicionar a pokedex
         e ver detalhes dos pokemons */ }
+        {mapPokemons}
         <button onClick={""}> Adicionar </button>
         <button onClick={IrParaDetailsPage}> Ver detalhes</button>
       </div>
